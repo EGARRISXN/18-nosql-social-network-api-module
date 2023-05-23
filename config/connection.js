38b0cfa724f76a99f2ba152b8a71f560f1f643a6
Ethan Garrison <egarrisxn@gmail.com>
2023-05-22 20:40:50 -0400

    working on models

--- config/connection.js
+++ config/connection.js
@@ -5,4 +5,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmed
     useUnifiedTopology: true
 });
 
+mongoose.set("debug", true);
+
 module.exports = mongoose.connection;
